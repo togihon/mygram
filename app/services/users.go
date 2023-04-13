65ff68788aeaa17644a9de6ae93d1f15cf9dce3d
Togi Simaremare <togihon@users.noreply.github.com>
2023-04-13 16:32:03 +0700

    Update users.go

--- app/services/users.go
+++ app/services/users.go
@@ -5,7 +5,6 @@ import (
 	"mygram/pkg/database"
 	"mygram/pkg/helpers"
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -117,7 +116,7 @@ func MyGramUserLogin(c *gin.Context) {
 		return
 	}
 
-	token := helpers.GenerateToken(User.ID, User.Email)
+	token := helpers.GenerateToken(User.ID, User.Email, User.Username, User.CreatedAt)
 
 	c.JSON(http.StatusOK, entity.ResponseLogin{
 		Success: true,
@@ -125,47 +124,5 @@ func MyGramUserLogin(c *gin.Context) {
 		Data: struct {
 			Token string "json:\"token\" example:\"eyJhbGciOiJI....\""
 		}{Token: token},
-	})
-}
-
-// MyGramGetUser godoc
-// @Summary Get User Data
-// @Description Getting user daa by id
-// @Tags users
-// @Consumes ({mpfd,json})
-// @Produce json
-// @Param id path int true "User's ID"
-// @Success 200 {object} entity.Response "Jika email dan password benar, maka akan mendapatkan token"
-// @Failure 401  {object}  entity.ResponseFailed "Jika email dan password salah, maka akan muncul error"
-// @Router /users/{id} [GET]
-func MyGramGetUser(c *gin.Context) {
-	db, _ := database.Connect()
-	contentType := helpers.GetContentType(c)
-	User := entity.MyGramUser{}
-
-	//get parameter
-	userID, _ := strconv.Atoi(c.Param("id"))
-
-	if contentType == appJSON {
-		c.ShouldBindJSON(&User)
-	} else {
-		c.ShouldBind(&User)
-	}
-
-	//query select * from users where id = param
-	err := db.First(&User, "id = ?", userID).Error
-
-	if err != nil {
-		c.JSON(http.StatusNotFound, entity.ResponseFailed{
-			Success: false,
-			Message: "User not found",
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, entity.Response{
-		Success: true,
-		Message: "User data has been loaded successfully",
-		Data:    User,
 	})
 }
